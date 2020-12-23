6a77558e44553e14ded71536788b5b8a38a2bb74
Azurelol <ckpsm@live.com>
2020-12-23 22:35:11 +0100

    Added Stratus-Core through UPM

---
+++ StratusFrameworkSample/Assets/Scripts/StratusUsageTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Stratus;
+
+namespace StratusTest
+{
+	public class StratusUsageTest : Stratus.StratusBehaviour
+	{
+		private void Awake()
+		{
+			StratusDebug.Log("Boop!");
+		}
+	}
+
+}
